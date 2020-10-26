235a8a60534c2fe3321151f0e2329671378d21b8
Brando Vidal <65886822+brandovidal@users.noreply.github.com>
2020-10-26 01:00:05 -0500

    update links

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 const Header = ({ name }) => (
   <header className="body-font fixed w-screen bg-gray-100 text-gray-900 z-10">
-    <div className="container mx-auto flex flex-wrap px-3 py-3 flex-col md:flex-row items-center ">
+    <div className="container mx-auto flex flex-wrap px-3 py-2 md:py-3 flex-col md:flex-row items-center ">
       <Link href="/">
-        <a className="flex title-font font-medium items-center text-gray-900 mr-3 mb-4 md:mb-0">
-          <span className="ml-3 text-xl">{name}</span>
+        <a className="flex title-font font-medium items-center text-gray-900 mr-3 mb-0">
+          <span className="ml-3 text-xl uppercase">{name}</span>
         </a>
       </Link>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
