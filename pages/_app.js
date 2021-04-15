b9eb3db8c838fadac48bb2602d9c4f5ec3552da7
Brando Vidal <yisusdracon@gmail.com>
2021-04-15 11:28:04 -0500

    Add Analythics
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,43 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title key="title">Brando Vidal</title>
         <meta key="description" name="description" content="Contactame!" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-        <link rel="manifest" href="/favicon/site.webmanifest"/>
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="theme-color" content="#ffffff"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z7LNPCFK06"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || []; const dataLayer =
+            window.dataLayer || []; function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-Z7LNPCFK06');`,
+          }}
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
